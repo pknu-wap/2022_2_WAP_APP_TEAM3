cabb4415784567096ba0722f6ffa6feb27f0df7e
yuni <seizethedayunhui@naver.com>
2022-11-17 16:36:47 +0900

    MyReservation_GridView 만듦

---
+++ lib/pages/post/my_reservation.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MyReservation extends StatefulWidget {
+  const MyReservation({Key? key}) : super(key: key);
+
+  @override
+  _MyReservationState createState() => _MyReservationState();
+}
+
+class _MyReservationState extends State<MyReservation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: GridView.count(
+        crossAxisCount: 3, //1 개의 행에 보여줄 item 개수
+        childAspectRatio: 1 / 2, //item 의 가로 1, 세로 2 의 비율
+        mainAxisSpacing: 10, //수평 Padding
+        crossAxisSpacing: 10, //수직 Padding
+        children: List.generate(9, (index) {  //item 의 반목문 항목 형성
+          return Container(
+            color: Colors.lightGreen,
+            child: Text(' Item : $index'),
+          );
+        }),
+      ),
+
+    );
+  }
+}
