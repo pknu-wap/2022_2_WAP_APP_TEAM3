7862c49c21e8459b9c31fd82c4237c4ad3cc8a45
Imiswest <ies9961@naver.com>
2022-11-21 13:39:49 +0900

    대출하기 버튼과 팝업 구현 / 선택 날짜에 따라 바로 반영되게 바꿔야함

--- lib/pages/post/rental_page.dart
+++ lib/pages/post/rental_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wap_library/pages/main/home_page.dart';
 import 'package:wap_library/pages/post/detail_page.dart';
 import 'package:get/get.dart';
 
@@ -80,14 +81,14 @@ class RentalDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(10),
+      padding: EdgeInsets.all(3.5),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
             width: 400, //가로 설정
             height: 280, //높이 설정
-            padding: EdgeInsets.only(
+            padding: const EdgeInsets.only(
                 left: 10.0, top: 10.0, right: 10.0, bottom: 10.0),
             margin: const EdgeInsets.all(10.0),
 
@@ -97,7 +98,8 @@ class RentalDetail extends StatelessWidget {
               borderRadius: BorderRadius.circular(20),
             ),
 
-            child: Column(children: <Widget>[
+            child: Column(
+              children: <Widget>[
               //둥근모서리 박스 안 구성요소
               const Text(
                 '대출 상세 설정',
@@ -116,6 +118,7 @@ class RentalDetail extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
+                    padding: EdgeInsets.only(top:8.0),
                     child: const Text(
                       '반납일을 선택해주세요.',
                       textAlign: TextAlign.left,
@@ -129,11 +132,11 @@ class RentalDetail extends StatelessWidget {
                 ],
               ),
 
-              Container( //반납일 선택 박스
+              Container( //반납일 관련 박스
                 width: 350, //가로 설정
                 height: 70, //높이 설정
                 padding: const EdgeInsets.only(
-                    left: 10.0, top: 10.0, right: 10.0, bottom: 10.0),
+                    left: 10.0, top: 1.0, right: 10.0, bottom: 1.0),
                 margin: const EdgeInsets.all(10.0),
 
                 decoration: BoxDecoration(//컨테이너 데코레이션
@@ -145,14 +148,21 @@ class RentalDetail extends StatelessWidget {
                 child: SelectDayButton(), //대출날짜 고르는 버튼
               ),
 
-              const SizedBox(
-                height: 50.0,
+
+              const Text('* 최대 대출 가능 기간은 1학기입니다.'),
+
+                const SizedBox(
+                  height: 12.0,
+                ),
+
+              Container(
+                child: RentalButton(),
               ),
 
               const Padding(
                 padding: EdgeInsets.only(
-                    left: 30.0, top: 10.0, right: 10.0, bottom: 10.0),
-              )
+                    left: 30.0, top: 10.0, right: 10.0, bottom: 0.0),
+              ),
             ]),
           ),
         ],
@@ -161,7 +171,7 @@ class RentalDetail extends StatelessWidget {
   }
 }
 
-///대출 날짜 고르는 버튼 + 선택에 따라 날짜가 수정되는 박스
+///반납일 고르는 버튼 + 선택에 따라 날짜가 수정되는 박스
 class SelectDayButton extends StatelessWidget {
   DateTime date = DateTime.now();
   @override
@@ -183,7 +193,7 @@ class SelectDayButton extends StatelessWidget {
             if (newDate == null) return;
 
             // if 'OK' => DateTime
-          },
+          }
         ),
 
         const SizedBox( // 사이 여백
@@ -205,6 +215,72 @@ class SelectDayButton extends StatelessWidget {
           ),
         ),
       ]
+    );
+  }
+}
+
+/// 대출하기 버튼
+class RentalButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      width: MediaQuery.of(context).size.width *0.77,
+      height: MediaQuery.of(context).size.height *0.06,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+
+        ///대출하기 버튼 누르면 뜨는 팝업
+        onPressed: (){
+          showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Icon(Icons.check_circle),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget> [
+                      Text("도서 대출이 완료되었습니다."),
+
+                      Container( //텍스트 밑의 둥근모서리 박스
+                        width: 210, //가로 설정
+                        height: 110, //높이 설정
+                        padding: EdgeInsets.all(15.0),
+                        margin: const EdgeInsets.all(10.0),
+
+                        child: Column( //둥근모서리 박스 안 구성요소
+                          children: <Widget>[
+                            OutlinedButton( // 버튼2
+                              style: OutlinedButton.styleFrom(
+                                  fixedSize: const Size(180,70)
+                              ),
+                              onPressed:() {Get.to(HomePage());},
+                              child: Text('홈페이지로 돌아가기'),
+                            ),
+                          ],
+                        ),
+
+                        decoration: BoxDecoration( //컨테이너 데코레이션
+                          border: Border.all(
+                              width: 3,
+                              color: Colors.lightGreen
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }
+          );
+        },
+        child: Text('대출하기'),
+      ),
     );
   }
 }
