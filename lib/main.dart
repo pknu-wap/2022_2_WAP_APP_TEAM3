2faa48812d6824a56b643a7bfde6d6b56f44b9dc
imiswest <ies9961@naver.com>
2023-02-04 20:52:27 +0900

    관리자 페이지 메뉴 디자인 수정

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:wap_library/pages/major/home_page.dart';
 import 'package:wap_library/pages/detail/detail_page.dart';
 import 'package:wap_library/pages/init/join_page.dart';
 import 'package:wap_library/pages/init/login_page.dart';
+import 'package:wap_library/pages/manager/manager_page.dart';
 
 import 'components/firestore_page.dart';
 
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home:SplashPage(), //여기다가 내가 가고싶은 페이지 이름 넣기
+      home:ManagerPage(), //여기다가 내가 가고싶은 페이지 이름 넣기
     );
   }
 }
--- lib/pages/manager/manager_page.dart
+++ lib/pages/manager/manager_page.dart
@@ -6,111 +6,165 @@ import 'remove_page.dart';
 
 ///관리자페이지
 class ManagerPage extends StatelessWidget {
+  PreferredSizeWidget CustomAppbar() {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(70.0), // here the desired height
+      child: AppBar(
+        backgroundColor: Color(0xff2D3C72),
+        title: Container(
+          padding: EdgeInsets.only(top: 20, left: 10),
+          child: Text(
+            "관리자 페이지",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Management"),
-        backgroundColor: Color(0xff006285),
-      ),
+      appBar: CustomAppbar(),
       body: Center(
-        child:
-        ///관리자메뉴박스
-        Container(
-          padding: EdgeInsets.all(25.0),
-            margin: EdgeInsets.fromLTRB(0, 0, 0, 50,),
-            width: 250,
-            height: 340,
-            decoration:
-                BoxDecoration(
-                  color: Color(0xff006285),
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.7),
-                      spreadRadius: 0,
-                      blurRadius: 5.0,
-                      offset: Offset(0, 10),
-                    )
-                  ]
-                ),
-            child: Column(
-
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.7,
+          height: MediaQuery.of(context).size.height * 0.6,
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Color(0xff2D3C72),
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.7),
+                spreadRadius: 0,
+                blurRadius: 5.0,
+                offset: Offset(0, 10),
+              ),
+            ],
+          ),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-
-                  SizedBox(
-                    height: 25.0,
-                  ),
-              const Text('관리자 메뉴',
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: Text(
+                    '관리자 메뉴',
                     style: TextStyle(
                       fontSize: 20.0,
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
-                    ),),
-                SizedBox(
-                  height: 20.0,
+                    ),
+                  ),
                 ),
+
                 ///도서추가버튼
-                FloatingActionButton.extended(
-                    onPressed: () {Get.to(AddPage());},
-                    label: Text("도서 추가하기",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.bold,
-                    ),),
-                    backgroundColor: Colors.white,
-                  icon: Icon(
-                      Icons.post_add_outlined,
-                    size: 30,
-                    color: Colors.black,
-                  )
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                ///도서삭제버튼
-                FloatingActionButton.extended(
-                    onPressed: () {Get.to(RemovePage());},
-                    label: Text("도서 삭제하기",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.bold,
-                      ),),
-                    backgroundColor: Colors.white,
-                    icon: Icon(
-                        Icons.delete_outlined,
-                      size: 30,
-                      color: Colors.black,
-                    )
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                ///도서 조회 버튼
-                FloatingActionButton.extended(
-                    onPressed: () {Get.to(SearchPage());},
-                    label: Text("도서 조회하기",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.bold,
-                      ),),
-                    backgroundColor: Colors.white,
-                    icon: Icon(
-                      Icons.manage_search_outlined,
-                      size: 30,
-                      color: Colors.black,
-                    )
+                GestureDetector(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      //컨테이너 데코레이션
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.post_add_outlined,
+                          size: 30,
+                          color: Colors.black,
+                        ),
+                        SizedBox(width: 15),
+                        Text(
+                          "도서 추가하기",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  onTap: () {
+                    Get.to(AddPage());
+                  },
                 ),
 
+                ///도서 삭제버튼
+                GestureDetector(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      //컨테이너 데코레이션
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.delete_outlined,
+                          size: 30,
+                          color: Colors.black,
+                        ),
+                        SizedBox(width: 15),
+                        Text(
+                          "도서 삭제하기",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  onTap: () {
+                    Get.to(RemovePage());
+                  },
+                ),
 
-          ]
+                ///도서 조회하기
+                GestureDetector(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      //컨테이너 데코레이션
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.manage_search_outlined,
+                          size: 30,
+                          color: Colors.black,
+                        ),
+                        SizedBox(width: 15),
+                        Text(
+                          "도서 조회하기",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  onTap: () {
+                    Get.to(SearchPage());
+                  },
+                ),
+              ]),
         ),
       ),
-
-    ),
     );
   }
 }
